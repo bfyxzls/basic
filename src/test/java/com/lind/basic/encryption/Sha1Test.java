a044b142bca442f95822a0470cf091c6ecfe3d03
zhangzhanling <zhangzl@i-counting.cn>
2019-07-20 10:17:17 +0800

    添加加密算法

---
+++ src/test/java/com/lind/basic/encryption/Sha1Test.java
@@ -0,0 +1,23 @@
+package com.lind.basic.encryption;
+
+import com.lind.basic.util.EncryptionUtils;
+import com.lind.basic.util.HmacUtils;
+import java.nio.charset.Charset;
+import org.junit.Test;
+
+public class Sha1Test {
+  @Test
+  public void sha1() {
+    System.out.println(EncryptionUtils.SHA("hello"));
+  }
+
+  @Test
+  public void hmac() throws Exception {
+    byte[] pwd = HmacUtils.hmacsha1Encrypt("hello", "lind");
+    String message = new String(pwd);
+    System.out.println(message);
+
+    byte[] hello = "hello".getBytes();
+    System.out.println(new String(hello));
+  }
+}
