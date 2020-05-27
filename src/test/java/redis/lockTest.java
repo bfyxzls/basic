6e6be94c864ff700bacdebe61e1fe7f8998aaddd
Lind <bfyxzls@sina.com>
2020-05-27 14:31:38 +0800

    å添加了redis锁

---
+++ src/test/java/redis/lockTest.java
@@ -0,0 +1,42 @@
+package redis;
+
+import com.lind.basic.config.JedisLock;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@ActiveProfiles("test")
+@Slf4j
+public class lockTest {
+    @Autowired
+    JedisLock jedisLock;
+
+    /**
+     * 分布锁测试.
+     */
+    @Test
+    public void distributeLock() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            new Runnable() {
+                @SneakyThrows
+                @Override
+                public void run() {
+                    Long generatorId = System.currentTimeMillis();
+                    if (jedisLock.tryLock("user", generatorId.toString(), 100)) {
+                        logger.info("{}线程拿到了锁", Thread.currentThread().getName());
+                        Thread.sleep(90);
+                        jedisLock.releaseLock("user", generatorId.toString());
+                    }
+                }
+            };
+        }
+        Thread.sleep(5000);
+    }
+}
